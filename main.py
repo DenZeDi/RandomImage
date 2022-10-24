8003187e359d8f5e96318323d114a565a846fe12
DenZeDi <dskostrov@miem.hse.ru>
2022-10-24 15:30:44 +0300

    Now main window contain a picture inside and centralized layout

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
 import sys
 import os
 from random_image import main
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QWidget, QVBoxLayout, QMessageBox
-
-
-def on_click():
-    main()
-
-    dlg = QMessageBox()
-    dlg.setWindowTitle("Error")
-    dlg.setText("Something went wrong!\nTry to restart the app..")
-
-    if os.path.exists('random_image.png'):
-        os.system('random_image.png')
-    else:
-        dlg.exec()
-        app.exit()
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
 
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.label = QLabel("Click on the button to see a random picture")
         self.setWindowTitle("My App")
-        self.setGeometry(1200, 100, 440, 280)
+        self.setWindowIcon(QIcon('icon.png'))
+        self.setFixedSize(1280, 720)
 
-        label = QLabel("Click on the button to see a random picture")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         button = QPushButton("Push me!")
-        button.clicked.connect(on_click)
+        button.setFixedSize(500, 35)
+
+        button.clicked.connect(self.clicked_button)
 
         layout = QVBoxLayout()
-        layout.addWidget(label)
+        layout.addWidget(self.label)
         layout.addWidget(button)
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
+
+    def clicked_button(self):
+        main()
+
+        dlg = QMessageBox()
+        dlg.setWindowTitle("Error")
+        dlg.setText("Something went wrong!\nTry to restart the app..")
+
+        if os.path.exists('random_image.png'):
+            pixmap = QPixmap('random_image.png')
+            self.label.adjustSize()
+            self.label.setPixmap(pixmap)
+        else:
+            dlg.exec()
+            app.exit()
 
 
 if __name__ == '__main__':
